d7044599fe03bfb9510e557918f7f7002131348b
anup-segu <anup.segu@gmail.com>
2016-05-18 13:04:23 -0700

    Add completed_sales index

---
+++ frontend/completed_sales_index.jsx
@@ -0,0 +1,51 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var CompletedSalesIndex = React.createClass({
+  getInitialState: function() {
+    return {
+      data: null
+    };
+  },
+
+  componentWillReceiveProps: function (newProps) {
+    this.setState({ data: newProps.data });
+  },
+
+  createList: function() {
+    var categories = Object.keys(this.state.data["categories"]);
+    var categoryID = categories[0];
+    var items = this.state.data["categories"]["items"];
+
+    var items_content = items.map(function (item) {
+      return (
+        <li key={item["id"]}>
+          <a href={item["url"]}>{item["title"]}</a>
+          <p>Sales Price: {item["price"]}</p>
+          <p>Ended on: {item["end_time"].slice(0,10)}</p>
+        </li>
+      );
+    });
+
+    return (
+      <ul>
+        { items_content }
+      </ul>
+    );
+
+  },
+
+
+  render: function() {
+    if (this.state.data) {
+      return (
+        this.createList();
+      );
+    } else {
+      return <div />;
+    }
+  }
+
+});
+
+module.exports = CompletedSalesIndex;
